653f4de8dfaf2c41830be7561dc4be4559f2fc96
ricleal <ricleal@gmail.com>
2015-02-24 21:09:43 -0500

    test with shapely - incomplete

--- src/model/models.py
+++ src/model/models.py
@@ -5,7 +5,8 @@ Created on Jan 29, 2015
 '''
 from abc import ABCMeta, abstractmethod
 
-from shapely.geometry import Point
+import shapely
+import shapely.geometry
 
 class IModel(object):
     '''
@@ -14,19 +15,40 @@ class IModel(object):
     __metaclass__ = ABCMeta
     
     @abstractmethod
-    def intertection_in(self, Point):
+    def intertection_in(self, ray):
         pass
-    
-    @abstractmethod
-    def intertection_out(self, Point):
-        pass
-
-
 
 
 class Cylinder(IModel):
-    def __init__( self, center, radius, height):
-        #todo
-        pass
+    def __init__( self, base, radius, height):
+        """
+        base - point
+        height - in Y
+        """
+        self.base = base.buffer(radius)
+        h = shapely.geometry.Point(base.x, base.y+height, base.z)
+        self.top = h.buffer(radius)
+        
+        
+    def intertection_in(self, ray):
+        return self.shape.intersection(ray)
+
+class Sphere(IModel):
+    def __init__( self, center, radius):
+        self.shape = center.buffer(radius)
+        
+    def intertection_in(self, ray):
+        return self.shape.intersection(ray)
+    
+
     
     
+if __name__ == '__main__':
+    center = shapely.geometry.Point(0,0,0)
+    radius = 10.0
+    sphere = Sphere(center, radius)
+    
+    line = shapely.geometry.LineString([[10,10,5], [0,0,0]])  
+    x = sphere.intertection_in(line)
+    
+    print x
